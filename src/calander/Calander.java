52a5818886dae70c0156f219204186d2085dccaa
Songjae Lee <mmqq2003@gmail.com>
2021-09-15 13:48:12 +0900

    Update calander main()

--- src/calander/Calander.java
+++ src/calander/Calander.java
@@ -3,7 +3,13 @@ package calander;
 public class Calander {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Calander");
+		System.out.println(" 일    월    화    수    목    금    토");
+		System.out.println("--------------------");
+		System.out.println("30 31  1  2  3  4  5");
+		System.out.println(" 6  7  8  9 10 11 12");
+		System.out.println("13 14 15 16 17 18 19");
+		System.out.println("20 21 22 23 24 25 26");
+		System.out.println("27 28 29 30  1  2  3"); // ctrl + shift + f를 누르면 자동 정렬
 	}
 
 }
